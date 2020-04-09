1f14b8d953bea5996f4f90fac18615aaa91753dc
miail-t <mihanik.timkov@gmail.com>
2020-04-10 04:06:03 +0600

    use transition for modal, add normalize.css

--- script/script.js
+++ script/script.js
@@ -2,43 +2,42 @@ let slider_container = document.getElementById("slider-container");
 let dropdown = document.getElementById("dropdown");
 let dropdown_control = document.getElementById("dropdown_control");
 
-function getBottomLine() {
+
+ function getBottomLine() {
   return slider_container.offsetHeight - dropdown_control.offsetHeight;
-};
+}
 
-function getUpperBound(){
+function getUpperBound() {
   return slider_container.offsetHeight - dropdown.offsetHeight;
-};
+}
 
-dropdown.style.left = (innerWidth-dropdown.offsetWidth-15)/2 + "px"
+dropdown.style.left = (innerWidth - dropdown.offsetWidth - 15) / 2 + "px";
 
-
-
-window.addEventListener("resize", function() {
-  dropdown.style.top = getBottomLine()+"px"
-  dropdown.style.left = (innerWidth-dropdown.offsetWidth)/2+"px" 
-
+window.addEventListener("resize", function () {
+  dropdown.style.top = getBottomLine() + "px";
+  dropdown.style.left = (innerWidth - dropdown.offsetWidth) / 2 + "px";
 });
 
-dropdown.style.top = getBottomLine()+"px"
+dropdown.style.top = getBottomLine() + "px";
 
-
-function isOpenDropDown(){
+function isOpenDropDown() {
   if (dropdown.offsetTop === getBottomLine()) {
-    let id = setInterval( function () {moveDropDown(id, "up")} );
+    let id = setInterval(function () {
+      moveDropDown(id, "up");
+    });
   } else {
-    let id = setInterval(function () {moveDropDown(id, "down")});
+    let id = setInterval(function () {
+      moveDropDown(id, "down");
+    });
   }
 
- function moveDropDown(id, type) {
+  function moveDropDown(id, type) {
     if (type === "up") {
-      dropdown.style.top = 
-        new Number(dropdown.style.top.replace("px", "")) - 1 + "px"
-      ;
+      dropdown.style.top =
+        new Number(dropdown.style.top.replace("px", "")) - 1 + "px";
     } else {
-      dropdown.style.top = 
-        new Number(dropdown.style.top.replace("px", "")) + 1 + "px"
-      ;
+      dropdown.style.top =
+        new Number(dropdown.style.top.replace("px", "")) + 1 + "px";
     }
     if (
       (dropdown.style.top.replace("px", "") == getUpperBound() &&
@@ -48,18 +47,17 @@ function isOpenDropDown(){
     ) {
       clearInterval(id);
     }
-  };
-};
+  }
+} 
 
-let openModalBtn = document.getElementById("btn-modal");
-let owerfloy = document.getElementById("owerflow");
-let modal = document.getElementById("modal");
-let closeModalBtn = document.getElementById("closeModal");
+const openModalBtn = document.getElementById("btn-modal");
+const owerfloy = document.getElementById("owerflow");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("closeModal");
 
 openModalBtn.onclick = function () {
-  owerfloy.className = owerfloy.className.concat(" block-registration ");
-  modal.className = modal.className.concat(" block-registration ");
-  modal.style.left = (innerWidth - modal.offsetWidth) / 2 + "px";
+  owerfloy.className = owerfloy.className.concat(" modal_activ");
+  modal.className = modal.className.concat(" registration-area_open");
 };
 
 owerfloy.onclick = function () {
@@ -70,7 +68,7 @@ closeModalBtn.onclick = function () {
   closeModal();
 };
 
-function closeModal()  {
-  owerfloy.className = owerfloy.className.replace("block-registration ", "");
-  modal.className = modal.className.replace("block-registration ", "");
-};
+function closeModal() {
+  owerfloy.className = owerfloy.className.replace("modal_activ", "");
+  modal.className = modal.className.replace("registration-area_open", "");
+}
